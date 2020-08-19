Added heat dispatch from GTs. Fixed units for PV. Issue still exists with turbine heat dispatch calculation, and chiller electric consumption.

TNSAgent/tns/Dash/dash_plots.py
@@ -79,6 +79,7 @@ for i, la in enumerate(localAssets):
         gt_options.append({'label': name, 'value': name})
         costs["elec"] += dbs[name]["Cost"]
         totals["elec_source"] += gas_turbines[name]['Electricity Dispatched']
+        totals["heat_source"] += gas_turbines[name]['Heat Dispatched']
         totals["gas"] += gas_turbines[name]['Gas Consumed']
         totals["cost"] += gas_turbines[name]['Cost']
     if model_type in ['Chiller']:
@@ -189,7 +190,7 @@ app.layout = html.Div(
                         html.Div(
                             [  # Graph 2 Title
                                 html.Div(
-                                    [html.H6("Electricity Dispatched (kW)", className="two-thirds graph__title"),
+                                    [html.H6("Electricity Dispatched (W)", className="two-thirds graph__title"),
                                      html.Div(
                                          [  # Checklist
                                              dcc.Checklist(
@@ -280,7 +281,7 @@ app.layout = html.Div(
                         html.Div(
                             [
                                 html.Div(
-                                    [html.H6("Fuel Consumed ()", className="graph__title"),
+                                    [html.H6("Fuel Consumed (ft^3 Natural Gas)", className="graph__title"),
                                      html.Div(
                                          [  # Checklist
                                              dcc.Checklist(
@@ -460,37 +461,34 @@ def elec_graph(gt_selector, pv_selector, chiller_selector, inflex_selector, flex
     total = pd.DataFrame(np.zeros(len(gas_turbines["GasTurbine1"]["TimeStamp"])))
     if gt_selector is not None:
         for name in gt_selector:
-            total[0] = total[0] + gas_turbines[name]["Electricity Dispatched"]
             trace = dict(
                 name=name,
                 type="scatter",
                 # line={"color": "#42C4F7"},
                 mode="lines",
-                y=gas_turbines[name]["Electricity Dispatched"],
+                y=gas_turbines[name]["Electricity Dispatched"]*1000,
                 x=gas_turbines[name]["TimeStamp"],
             )
             data.append(trace)
     if pv_selector is not None:
         for name in pv_selector:
-            total[0] = total[0] + pvs[name]["Electricity Dispatched"]
             trace = dict(
                 name=name,
                 type="scatter",
                 # line={"color": "#42C4F7"},
                 mode="lines",
-                y=pvs[name]["Electricity Dispatched"],
+                y=pvs[name]["Electricity Dispatched"]*1000,
                 x=pvs[name]["TimeStamp"],
             )
             data.append(trace)
     if chiller_selector is not None:
         for name in chiller_selector:
-            total[0] = total[0] + chillers[name]["Electricity Consumed"]
             trace = dict(
                 name=name,
                 type="scatter",
                 # line={"color": "#42C4F7"},
                 mode="lines",
-                y=chillers[name]["Electricity Consumed"],
+                y=chillers[name]["Electricity Consumed"]*1000,
                 x=chillers[name]["TimeStamp"],
             )
             data.append(trace)
@@ -500,19 +498,18 @@ def elec_graph(gt_selector, pv_selector, chiller_selector, inflex_selector, flex
                 name=name,
                 type="scatter",
                 mode="lines",
-                y=inflexibles[name]["Electricity Dispatched"],
+                y=inflexibles[name]["Electricity Dispatched"]*1000,
                 x=inflexibles[name]["TimeStamp"],
             )
             data.append(trace)
     if flex_selector is not None:
         for name in flex_selector:
-            total[0] = total[0] + flexibles[name]["Electricity Dispatched"]
             trace = dict(
                 name=name,
                 type="scatter",
                 # line={"color": "#42C4F7"},
                 mode="lines",
-                y=flexibles[name]["Electricity Dispatched"],
+                y=flexibles[name]["Electricity Dispatched"]*1000,
                 x=flexibles[name]["TimeStamp"],
             )
             data.append(trace)
@@ -526,7 +523,7 @@ def elec_graph(gt_selector, pv_selector, chiller_selector, inflex_selector, flex
                 name=label,
                 type="scatter",
                 mode="lines",
-                y=totals[name],
+                y=totals[name]*1000,
                 x=totals["TimeStamp"],
             )
             data.append(trace)

TNSAgent/tns/gas_turbine.py
@@ -123,7 +123,7 @@ class GasTurbine(LocalAssetModel):
             capacity = capacity[capacity!=0]
             kWhtocft = 3.41 # 3.41 cubic feet of natural gas per kWh
             # convert efficiency to fuel use
-            fuel_use = 1/efficiency*capacity*size*kWhtocft #convert to cubic feet of natural gas 
+            fuel_use = 1/efficiency*capacity*size*kWhtocft  # convert to cubic feet of natural gas
             # un-normalize the capacity data and remove (0,0) points. We don't want to fit to 0,0, because it is discontinuous
             capacity = size*capacity[fuel_use!=0]
             self.min_capacity = min(capacity)
@@ -148,12 +148,12 @@ class GasTurbine(LocalAssetModel):
             #     coefs_h_binned = np.flip(np.polyfit(heat, cap, 2))
             #     coefs_e.append(coefs_e_binned)
             #     coefs_h.append(coefs_h_binned)
-            regression_order = 4 # fourth oder regression should capture curve with high enough accuracy
-            coefs_e = np.flip(np.polyfit(capacity, fuel_use, regression_order),0)
-            coefs_h = np.flip(np.polyfit(heat_recovered, fuel_use, regression_order),0)
+            regression_order = 4  # fourth oder regression should capture curve with high enough accuracy
+            coefs_e = np.flip(np.polyfit(capacity, fuel_use, regression_order), 0)
+            coefs_h = np.flip(np.polyfit(heat_recovered, fuel_use, regression_order), 0)
         except:
-            coefs_e = [0, 2] # if there is no data start with an assumed efficiency of 50%
-            coefs_h = [0, 1/2] # if there is no data start with an assumed waste heat 2* power out
+            coefs_e = [0, 2]  # if there is no data start with an assumed efficiency of 50%
+            coefs_h = [0, 1/2]  # if there is no data start with an assumed waste heat 2* power out
         #save values
         self.fit_curve['coefs_e'] = coefs_e
         self.fit_curve['coefs_h'] = coefs_h
@@ -198,7 +198,8 @@ class GasTurbine(LocalAssetModel):
     def update_dispatch(self, mkt, fed=None, helics_flag=bool(0)):
 
         elec_dispatched = self.scheduledPowers[str(MeasurementType.PowerReal)]
-        gas_consumed = self.use_fit_curve(self.fit_curve['coefs_e'],elec_dispatched)
+        gas_consumed = self.use_fit_curve(self.fit_curve['coefs_e'], elec_dispatched)
+        heat_dispatched = self.use_fit_curve(self.fit_curve['coefs_h'], elec_dispatched)
         cost = mkt.gas_rate * gas_consumed
 
         if helics_flag == True:
@@ -222,11 +223,13 @@ class GasTurbine(LocalAssetModel):
             self.record['TimeStamp']=[]
             self.record['TimeInterval']=[]
             self.record['Electricity Dispatched']=[]
+            self.record['Heat Dispatched']=[]
             self.record['Gas Consumed']=[]
             self.record['Cost']=[]
         self.record['TimeStamp'].append(str(mkt.marketClearingTime))
         self.record['TimeInterval'].append(str(interval))
         self.record['Electricity Dispatched'].append(str(elec_dispatched))
+        self.record['Heat Dispatched'].append(str(heat_dispatched))
         self.record['Gas Consumed'].append(str(gas_consumed))
         self.record['Cost'].append(str(cost))
         # line_new =  str(mkt.marketClearingTime) + "," + str(interval) + "," + str(elec_dispatched) +  "," + str(gas_consumed) + "," + str(cost) + " \n"